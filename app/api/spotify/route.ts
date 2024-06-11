f11707b6b5696a4d0b6af300576544a58df00992
Favour <ayofavour7@gmail.com>
2024-06-11 21:18:35 +0100

    fix: add spotify integration

---
+++ app/api/spotify/route.ts
@@ -0,0 +1,132 @@
+import Airtable from 'airtable';
+
+export const GET = async (request) => {
+  try {
+    // Configure Airtable
+    Airtable.configure({
+      endpointUrl: 'https://api.airtable.com',
+      apiKey: process.env.AIRTABLE_API_KEY,
+    });
+    const base = Airtable.base(process.env.AIRTABLE_BASE_ID!);
+
+    // Get token data from Airtable
+    const records = await base('token').select().firstPage();
+    let token = records[0].fields;
+
+    const now = Date.now();
+    // @ts-ignore
+    const tokenValid = token.created + token.expiry > now;
+
+    if (!tokenValid) {
+      token = await getAccessToken(token.refresh_token);
+      await updateToken(base, token);
+    }
+
+    const playingDetails = await getNowPlaying(token.token);
+
+    return new Response(JSON.stringify(playingDetails), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+};
+
+async function getAccessToken(refreshToken) {
+  const basic = Buffer.from(
+    process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+  ).toString('base64');
+
+  const headers = {
+    Authorization: `Basic ${basic}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  const params = new URLSearchParams();
+  params.append('grant_type', 'refresh_token');
+  params.append('refresh_token', refreshToken);
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers,
+    body: params,
+  });
+
+  const data = await response.json();
+  return transformToken(data);
+}
+
+async function updateToken(base, token) {
+  await base('token').update([
+    {
+      id: process.env.AIRTABLE_RECORD_ID,
+      fields: token,
+    },
+  ]);
+}
+
+function transformToken(token) {
+  return {
+    token: token.access_token,
+    expiry: (token.expires_in - 300) * 1000,
+    created: Date.now(),
+  };
+}
+
+async function getNowPlaying(accessToken) {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  let songName = '';
+  let isPlaying = false;
+  let artistName = '';
+  let url = '';
+  let coverImageUrl = '';
+
+  try {
+    const res = await fetch(
+      'https://api.spotify.com/v1/me/player/currently-playing',
+      {
+        headers,
+      }
+    );
+
+    const data = await res.json();
+
+    if (!data.is_playing || data.currently_playing_type !== 'track') {
+      const res = await fetch(
+        'https://api.spotify.com/v1/me/player/recently-played?limit=1',
+        {
+          headers,
+        }
+      );
+
+      const recentData = await res.json();
+      const recentTrack = recentData.items[0].track;
+      songName = recentTrack.name;
+      isPlaying = false;
+      artistName = recentTrack.artists[0].name;
+      url = recentTrack.external_urls.spotify;
+      coverImageUrl = recentTrack.album.images[0].url;
+    } else {
+      const track = data.item;
+      songName = track.name;
+      isPlaying = data.is_playing;
+      artistName = track.artists[0].name;
+      url = track.external_urls.spotify;
+      coverImageUrl = track.album.images[0].url;
+    }
+
+    return { artistName, isPlaying, songName, url, coverImageUrl };
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to fetch now playing details');
+  }
+}
---
+++ app/hero.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+const Hero = () => {
+  const [playingDetails, setPlayingDetails] = useState({
+    url: '',
+    artist: '',
+    songName: '',
+    coverImageUrl: '',
+  });
+
+  useEffect(() => {
+    const fetchPlayingDetails = () => {
+      fetch('/api/spotify')
+        .then((res) => res.json())
+        .then((data) => {
+          setPlayingDetails({
+            url: data.url,
+            artist: data.artistName,
+            songName: data.songName,
+            coverImageUrl: data.coverImageUrl,
+          });
+        })
+        .catch((error) => {
+          console.error('Error fetching playing details:', error);
+        });
+    };
+
+    fetchPlayingDetails();
+
+    const intervalId = setInterval(fetchPlayingDetails, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <section className='hero'>
+        <div className='hero__image'>
+          <Image src='/proton.png' alt='hero' width={400} height={400} />
+          <div className='placeholder'></div>
+        </div>
+        <div className='hero__text'>
+          {playingDetails.artist &&
+            playingDetails.songName &&
+            playingDetails.url &&
+            playingDetails.coverImageUrl && (
+              <a
+                href={playingDetails.url}
+                className='now-playing auto-scroll-container'
+              >
+                <div className='auto-scroll-content'>
+                  <span className='bold'>NOW PLAYING: </span>
+                  <span>
+                    {playingDetails.songName} BY {playingDetails.artist}
+                  </span>
+                </div>
+              </a>
+            )}
+          <h1>
+            Bolaji <br /> Olajide
+          </h1>
+        </div>
+      </section>
+
+      <section className='hero__about'>
+        <p>
+          I’m a full-stack software engineer based in Lagos, Nigeria and I have
+          over 7 years of experience. I’m currently increasing developer
+          productivity at{' '}
+          <a
+            href='https://sourcegraph.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Sourcegraph
+          </a>
+          . You can check out my{' '}
+          <a
+            href='http://github.com/BolajiOlajide'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            GitHub
+          </a>{' '}
+          here.
+        </p>
+
+        <p>
+          By night, my alter-ego, Proton, entertains people with my DJ sets and
+          original music productions. My sound, a fusion of electronic, house,
+          and techno, reflects my engineering precision and creative flair.
+          Proton has performed at various clubs and music festivals. Listen to
+          his tracks on{' '}
+          <a target='_blank' href=''>
+            SoundCloud
+          </a>
+          .
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
