5e5fca527544d49c0be0cb190040b08b4f9fa17c
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-18 16:37:07 +0100

    first pass at update with correct links

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 
 import { cx } from 'misc/utils';
-import { PROJECTS } from 'misc/data';
+import { MUSIC } from 'misc/data';
 import StyledText from 'app/components/styled-text';
 
 export const metadata = {
@@ -9,7 +9,7 @@ export const metadata = {
   description: 'Some of the music I have worked on.',
 };
 
-export default function Uses() {
+export default function Music() {
   const generateSpanSequence = (n) => {
     const differences = [4, 4, 6, 6, 4];
     let sequence = [3];
@@ -23,7 +23,8 @@ export default function Uses() {
     return sequence;
   };
 
-  const sequence = useMemo(() => generateSpanSequence(30), [PROJECTS]);
+  const sequence = useMemo(() => generateSpanSequence(MUSIC.length), [MUSIC]);
+  console.log(sequence.length, '<===');
 
   return (
     <section className='page__container'>
@@ -31,13 +32,17 @@ export default function Uses() {
         <h1>Music</h1>
       </header>
       <section className='music'>
-        {[...Array(30).fill(null)].map((project, idx) => (
-          <div
-            className={cx('project', sequence.includes(idx + 1) && 'span-2')}
+        {MUSIC.map((m, idx) => (
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href={m.link}
+            className={cx('music__node', sequence.includes(idx + 1) && 'span-2')}
+            style={{'backgroundImage': `url('${m.image}')`}}
             key={idx}
           >
-            {idx + 1}
-          </div>
+            <span className='music__node-text'>{m.name}</span>
+          </a>
         ))}
       </section>
     </section>
