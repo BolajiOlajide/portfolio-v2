dbc9e909e49e254b294b868837844512c67b5773
Favour <ayofavour7@gmail.com>
2024-06-20 11:00:05 +0100

    feat: allow readers to clear tag

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,18 @@
-import { BlogPosts } from 'app/components/posts';
+import { BlogPosts } from "app/components/posts";
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: "Blog",
+  description: "Read my blog.",
 };
 
 export default function Page() {
   return (
-    <section className='container'>
-      <div className='blog'>
-        <h2>Articles</h2>
-        <BlogPosts  />
+    <section className="container">
+      <div className="blog">
+        <header className="blog__header">
+          <h2>Articles</h2>
+        </header>
+        <BlogPosts />
       </div>
     </section>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import Link from 'next/link';
-import { ArrowTopRightIcon } from '@radix-ui/react-icons';
+import Link from "next/link";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
-import Hero from './hero';
-import { BlogPosts } from 'app/components/posts';
+import Hero from "./hero";
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
-    <div className='container'>
+    <div className="container">
       <Hero />
 
-      <section className='recent-posts'>
-        <div className='recent-posts__header'>
+      <section className="recent-posts">
+        <div className="recent-posts__header">
           <h2>Recent Posts</h2>
 
-          <Link href='/blog'>
+          <Link href="/blog">
             Read More <ArrowTopRightIcon />
           </Link>
         </div>
