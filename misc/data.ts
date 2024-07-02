9e2f022aeea3d6e961c3d7fe3f5d8e95bac97f3e
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-07-02 01:13:46 +0100

    format

--- misc/data.ts
+++ misc/data.ts
@@ -4,45 +4,47 @@ export const PROJECTS: Array<{
   name: string;
 }> = [
   {
-    name: 'Annie',
+    name: "Annie",
     description:
-      'Imagine a world where you can share your favorite songs and playlists without having to bother about which streaming platform you are using. That\'s the world Annie is building.',
-    link: 'https://anniemusic.app/',
+      "Imagine a world where you can share your favorite songs and playlists without having to bother about which streaming platform you are using. That's the world Annie is building.",
+    link: "https://anniemusic.app/",
   },
   {
-    name: 'now-playing',
+    name: "now-playing",
     description:
-      'This library allows you to get the currently playing song on your streaming platform, it currently supports Spotify.',
-    link: 'https://github.com/BolajiOlajide/now-playing',
+      "This library allows you to get the currently playing song on your streaming platform, it currently supports Spotify.",
+    link: "https://github.com/BolajiOlajide/now-playing",
   },
   {
-    name: 'ng-faker',
+    name: "ng-faker",
     description:
-      'ng-faker is a javascript library that is used to generate fake data for testing your applications. It is based off the Faker library and used to generate data that is pertaining to the nigerian market.',
-    link: 'https://github.com/BolajiOlajide/ng-faker',
+      "ng-faker is a javascript library that is used to generate fake data for testing your applications. It is based off the Faker library and used to generate data that is pertaining to the nigerian market.",
+    link: "https://github.com/BolajiOlajide/ng-faker",
   },
   {
-    name: 'koii',
+    name: "koii",
     description:
-      'koii is an express middleware that is used in development to print the available routes in an express application to the console.',
-    link: 'https://github.com/BolajiOlajide/koii',
+      "koii is an express middleware that is used in development to print the available routes in an express application to the console.",
+    link: "https://github.com/BolajiOlajide/koii",
   },
   {
-    name: 'kaffeine',
+    name: "kaffeine",
     description:
-      'Kaffeine is a simple CLI tool that continously pings your servers to keep it alive. It\'s particularly useful when running projects on providers that shut down your app due to inactivity after a certain period of time.',
-    link: 'https://github.com/BolajiOlajide/kaffeine',
+      "Kaffeine is a simple CLI tool that continously pings your servers to keep it alive. It's particularly useful when running projects on providers that shut down your app due to inactivity after a certain period of time.",
+    link: "https://github.com/BolajiOlajide/kaffeine",
   },
   {
-    name: 'folly',
-    description: 'folly keeps tracks of all slack reactions to a message, so you can request for it and view',
-    link: 'https://github.com/BolajiOlajide/folly',
+    name: "folly",
+    description:
+      "folly keeps tracks of all slack reactions to a message, so you can request for it and view",
+    link: "https://github.com/BolajiOlajide/folly",
   },
   {
-    name: 'ng-banks',
-    description: 'ng-banks is a library that allows you to easily fetch bank accounts and transactions from the nigerian market.',
-    link: 'https://github.com/BolajiOlajide/ng-banks',
-  }
+    name: "ng-banks",
+    description:
+      "ng-banks is a library that allows you to easily fetch bank accounts and transactions from the nigerian market.",
+    link: "https://github.com/BolajiOlajide/ng-banks",
+  },
 ];
 
 export const MUSIC: Array<{
@@ -51,53 +53,62 @@ export const MUSIC: Array<{
   name: string;
 }> = [
   {
-    image: 'https://i1.sndcdn.com/artworks-JEi8Zy4ojrho7E4z-J7Wq1A-t500x500.jpg',
-    link: 'https://soundcloud.com/protonmakesmusic/beta-mix-8',
-    name: 'Beta Mix #8'
+    image:
+      "https://i1.sndcdn.com/artworks-JEi8Zy4ojrho7E4z-J7Wq1A-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-8",
+    name: "Beta Mix #8",
   },
   {
-    image: 'https://i1.sndcdn.com/artworks-gWflgR5ISUZLziIP-4R01ZQ-t500x500.jpg',
-    link: 'https://soundcloud.com/protonmakesmusic/beta-mix-7',
-    name: 'Beta Mix #7'
+    image:
+      "https://i1.sndcdn.com/artworks-gWflgR5ISUZLziIP-4R01ZQ-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-7",
+    name: "Beta Mix #7",
   },
   {
-    image: 'https://i1.sndcdn.com/artworks-cqu5CpS2cYOpyubM-fftADQ-t500x500.jpg',
-    link: 'https://soundcloud.com/protonmakesmusic/beta-mix-6',
-    name: 'Beta Mix #6'
+    image:
+      "https://i1.sndcdn.com/artworks-cqu5CpS2cYOpyubM-fftADQ-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-6",
+    name: "Beta Mix #6",
   },
   {
-    image: 'https://i1.sndcdn.com/artworks-5EOx0HjZxzyBDwBz-G2pMwQ-t500x500.jpg',
-    link: 'https://soundcloud.com/protonmakesmusic/summer-house-party-1',
-    name: 'Summer House Party #1'
+    image:
+      "https://i1.sndcdn.com/artworks-5EOx0HjZxzyBDwBz-G2pMwQ-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/summer-house-party-1",
+    name: "Summer House Party #1",
   },
   {
-    image: 'https://i1.sndcdn.com/artworks-fiPzCyBerHlsIrqo-b3ZEBQ-t500x500.jpg',
-    link: 'https://soundcloud.com/protonmakesmusic/beta-mix-5-deep-house',
-    name: 'Beta Mix #5'
+    image:
+      "https://i1.sndcdn.com/artworks-fiPzCyBerHlsIrqo-b3ZEBQ-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-5-deep-house",
+    name: "Beta Mix #5",
   },
   {
-    image: 'https://i1.sndcdn.com/artworks-1gKYkzoOyVHZUKYz-gQqqmg-t500x500.jpg',
-    link: 'https://soundcloud.com/protonmakesmusic/beta-mix-4-afrohousehouse',
-    name: 'Beta Mix #4 (Afrohouse/House)'
+    image:
+      "https://i1.sndcdn.com/artworks-1gKYkzoOyVHZUKYz-gQqqmg-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-4-afrohousehouse",
+    name: "Beta Mix #4 (Afrohouse/House)",
   },
-    {
-      image: 'https://i1.sndcdn.com/artworks-gdsHCkyEmvMqLGBP-RyfrRQ-t500x500.jpg',
-      link: 'https://soundcloud.com/protonmakesmusic/beta-mix-3',
-      name: 'Beta Mix #3 (3-step, blakkitchen)'
-    },
-    {
-      image: 'https://i1.sndcdn.com/artworks-cwHt0v2m3ZuykQP2-susm3g-t500x500.jpg',
-      link: 'https://soundcloud.com/protonmakesmusic/beta-mix-2-amapiano',
-      name: 'Beta Mix #2 (Amapiano)'
-    },
-    {
-      image: 'https://i1.sndcdn.com/avatars-dEXViwkbl27QrA4z-XMxk9g-t500x500.jpg',
-      link: 'https://soundcloud.com/protonmakesmusic/performance-reviews',
-      name: 'Performance Reviews (Afrohouse Mix)'
-    },
-    {
-      image: 'https://i1.sndcdn.com/artworks-cwHt0v2m3ZuykQP2-susm3g-t500x500.jpg',
-      link: 'https://soundcloud.com/protonmakesmusic/beta-mix-1',
-      name: 'Beta Mix #1'
-    },
-  ];
+  {
+    image:
+      "https://i1.sndcdn.com/artworks-gdsHCkyEmvMqLGBP-RyfrRQ-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-3",
+    name: "Beta Mix #3 (3-step, blakkitchen)",
+  },
+  {
+    image:
+      "https://i1.sndcdn.com/artworks-cwHt0v2m3ZuykQP2-susm3g-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-2-amapiano",
+    name: "Beta Mix #2 (Amapiano)",
+  },
+  {
+    image: "https://i1.sndcdn.com/avatars-dEXViwkbl27QrA4z-XMxk9g-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/performance-reviews",
+    name: "Performance Reviews (Afrohouse Mix)",
+  },
+  {
+    image:
+      "https://i1.sndcdn.com/artworks-cwHt0v2m3ZuykQP2-susm3g-t500x500.jpg",
+    link: "https://soundcloud.com/protonmakesmusic/beta-mix-1",
+    name: "Beta Mix #1",
+  },
+];
