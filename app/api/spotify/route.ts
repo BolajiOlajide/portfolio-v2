e2d38a2221d9e3e981e3f480b362e9c7df12080e
Favour <ayofavour7@gmail.com>
2024-12-26 02:23:47 +0100

    fix: add workaround for spotify preview url bug

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -1,21 +1,25 @@
 import { NowPlaying, Providers } from "@bolajiolajide/now-playing";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export const GET = async () => {
   try {
-    if (process.env.NODE_ENV !== 'production') {
-      return new Response(JSON.stringify({
-        title: 'Test Song (Non-production env)',
-        artiste: 'Chronixx',
-        image_url: 'https://i.scdn.co/image/ab67616d0000b273fb3d081418a02919fb1bb58f',
-        is_playing: true,
-        preview_url: 'https://p.scdn.co/mp3-preview/2d7798a99d7c4091da509418b6b752463d569547?cid=98dee14da74049fdbc531633fec2f9ae',
-        url: ''
-      }), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
+    if (process.env.NODE_ENV !== "production") {
+      return new Response(
+        JSON.stringify({
+          title: "Test Song (Non-production env)",
+          artiste: "Chronixx",
+          image_url:
+            "https://i.scdn.co/image/ab67616d0000b273fb3d081418a02919fb1bb58f",
+          is_playing: true,
+          preview_url: null,
+          url: "https://open.spotify.com/track/0IFwcLA3vdeS9IxypgiQ8N",
+        }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
     }
 
     const nowPlaying = new NowPlaying(Providers.SPOTIFY, {
